d5f37bb75a653823199394fafc9ace781286358f
Ugo Vollhardt <ugo.vollhardt@protonmail.com>
2019-07-20 12:08:51 +0200

    Update of mainwindow
    The main window take care now of the connection to the database, as well a the creation of the document model and its connection with its specific view, and trigger the creation of a c_add_document GUI via the menu for the creation of a new document.
    The insertion of the new document in the model is not handle at the moment.

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -5,10 +5,33 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    // =========== Mise en place de l'interface ===========
     ui->setupUi(this);
+
+    // ========= Création BDD  =========
+    database_manager.open(QString("../../bdd/database.db"));
+
+    // =========== Gestion Documents ===========
+    document_model = new c_document_model(&database_manager,this);
+    ui->tableView_documents->setModel(document_model);
+
+    ui->tableView_documents->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableView_documents->resizeColumnsToContents();
+    ui->tableView_documents->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView_documents->setShowGrid(true);
+    ui->tableView_documents->sortByColumn(0,Qt::AscendingOrder);
+
+    // =========== Connexions ===========
+    QObject::connect(ui->action_add_document,SIGNAL(triggered()),this,SLOT(on_add_document()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_add_document() {
+    c_add_document gui(&database_manager);
+    int res = gui.exec();
+    qDebug() << gui.get_checked_tags().size();
 }
--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <c_dbmanager.h>
+#include <c_document.h>
+#include <c_document_model.h>
+#include <c_tag.h>
+#include <c_add_document.h>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,14 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void on_add_document();
+    //void on_doubleCliked(QModelIndex index);
+
 private:
     Ui::MainWindow *ui;
+    c_dbManager database_manager;
+    c_document_model* document_model;
 };
 
 #endif // MAINWINDOW_H
